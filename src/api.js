ee80972d5c35456c725378520d97a287fcc1ff32
Aki <vinceaki11@gmail.com>
2023-06-16 23:26:36 +0800

    test

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://server-chatgpt-topaz.vercel.app/', { 
+        // https://server-chatgpt-topaz.vercel.app/
+        console.log(chat.map((message)=> message.message).join(" \n "))
+        const response = await fetch('https://server-chatgpt-topaz.vercel.app', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
